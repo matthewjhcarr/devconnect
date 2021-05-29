05c468974dc1eb8f8c21f1f4755edcb01ed7bdf6
Matthew Carr <matthewjhcarr@gmail.com>
2021-05-28 20:07:21 -0400

    Section 9: Dashboard and Profile
    added getCurrentProfile action, get_profile and profile_error action types, a profile reducer, and aupdated the dashboard component to use the action types, profile reducer, and updated Dashboard component to use getCurrentProfile

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,31 @@
+import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {}
+};
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+
+  // Handles the incoming action (receives the type and payload of an action)
+  switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      };
+    default:
+      return state;
+  }
+}
