74da15b7436d2edbc2d0f906ec369c2193c52730
Matthew Carr <matthewjhcarr@gmail.com>
2021-06-01 08:14:20 -0400

    Section 9: Profile & Dashboard
    Added the ability to create profiles
    - Added the createProfile action, which can be used to create or update profiles
    - Added the CreateProfile component, which determines the layout and behaviour
      of the Create Profile form

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -20,3 +20,44 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+// Create or update profile
+// If you wanted to, you could create a separate function for updating/editing and you would just remove the edit param from this function
+export const createProfile =
+  (formData, history, edit = false) =>
+  async (dispatch) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+
+      const res = await axios.post('/api/profile', formData, config);
+
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      });
+
+      dispatch(
+        setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success')
+      );
+
+      if (!edit) {
+        // This is how redirecting is done within an action. We cannot use the Redirect class we use in component files
+        history.push('/dashboard');
+      }
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
